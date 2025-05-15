932b4405509b996bdea11d5af7b04df2c6f922e9
Cyzo50218 <myfavoritemappingswar@gmail.com>
2025-05-16 01:58:31 +0800

    Fixed error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,5 +70,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     
-    implementation("com.github.Dimezis:BlurView:2.0.6")
+    implementation("com.github.Dimezis:BlurView:version-2.0.6")
 }
